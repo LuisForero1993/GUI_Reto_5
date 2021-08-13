413ec110350f0454e486c8c22e83106279dd879c
Luis Alberto Forero Rincon <lforerom29@gmmail.com>
2021-08-12 21:10:14 -0500

    Conexion a base de datos

--- src/main/java/co/edu/utp/misiontic2022/c2/lforero/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/lforero/App.java
@@ -1,13 +1,18 @@
 package co.edu.utp.misiontic2022.c2.lforero;
 
-/**
- * Hello world!
- *
- */
+import java.sql.SQLException;
+import co.edu.utp.misiontic2022.c2.lforero.util.JDBCUtilities;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            var conn = JDBCUtilities.getConnection();
+            System.out.println("Conexión exitosa");
+            conn.close();
+        }catch (SQLException e){
+            System.err.println("Error: " + e);
+        }
     }
 }
